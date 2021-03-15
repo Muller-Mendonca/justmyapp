1baaae90e7c5a15a87e1ccf7095c923285c7270f
Muller Mendonça <mullerm.oficial@gmail.com>
2021-03-15 11:24:09 -0300

    colocando estilos ao produtos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Produtos from './Produtos';
 
+
 function App() {
   return (
     <div className="App">
--- src/Produtos.js
+++ src/Produtos.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './Produtos.modules.css'
+import styles from './Produtos.module.css'
 
 const Produtos = ({nome, valor}) => {
     return (
