bb38de6ce03ba9033830ceaf29235c8cc83f4818
Muller Mendonça <mullerm.oficial@gmail.com>
2021-03-17 09:37:35 -0300

    adicionando mais valores ao JSON

--- src/ProdutosJson.js
+++ src/ProdutosJson.js
@@ -6,5 +6,13 @@ export const json = [
     {
       "nome": "Homem Aranha",
       "valor": "R$ 79,99"
-    }
+    },
+    {
+      "nome": "Barbie",
+      "valor": "R$ 27,99"
+    },
+    {
+      "nome": "Toy Story Woody",
+      "valor": "R$ 12,99"
+    },
 ]
