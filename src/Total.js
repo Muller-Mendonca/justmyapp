6ce197cfcd53c8866f5265653db0882c4d8c8f77
Muller Mendonça <mullerm.oficial@gmail.com>
2021-03-23 09:37:50 -0300

    adicionando somador dos preços nos carrinhos

---
+++ src/Total.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Total.module.css'
+
+const Total = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Total
