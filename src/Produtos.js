aaafe5e4fe7a44aba2da8b4e77f8a88387cdadb1
Muller Mendonça <mullerm.oficial@gmail.com>
2021-04-24 14:45:30 -0300

    refatorando

--- src/Produtos.js
+++ src/Produtos.js
@@ -18,17 +18,6 @@ const Produtos = () => {
                      <h4>{data.valor * global.counter}</h4>
                   </div>
                 )
-
-                // FALTA   JOGAR O COUNTER AQUI E FAZER A MUTIPLICAÇAO PELO VALOR
-
-  //              const global = React.useContext(GlobalContext)
-//
-  //  return (
-  //      <div className={styles.content}>
-  //          <button className={styles.diminuir} onClick={global.diminuir}>-</button>
-  //          <p className={styles.theCounter}>{global.counter}</p>
-  //          <button className={styles.somar} onClick={global.somar}>+</button>
-  //      </div>
             })}
             
         </div>
